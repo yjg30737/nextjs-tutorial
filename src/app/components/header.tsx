30bbeef9edc8e6cfee7c8deeb49a7faf67eaaf14
Jung Gyu Yoon <yjg30737@gmail.com>
2023-01-15 14:15:07 +0900

    Set the index.tsx as the homepage instead of page.tsx, add the search bar on the header(only the UI)

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,23 @@
+import { AppProps } from "next/app"
 import React from "react"
 
 export function Header({ header } : { header: string }) {
   return <header className="bg-blue-500 p-4 shadow-md">
           <div className="container flex mx-auto items-center justify-between">
             <h1 className="mx-auto text-2xl font-bold text-white">{header}</h1>
+            <form className="relative">
+              <input
+                className="bg-white text-blue-700 rounded-full px-4 py-2 w-64 focus:outline-none focus:shadow-outline-blue"
+                type="text"
+                placeholder="Search"
+              />
+              <button
+                className="absolute top-0 right-0 bg-blue-400 text-white px-4 py-2 rounded-full hover:bg-blue-700"
+                type="submit"
+              >
+                Search
+              </button>
+            </form>
             <nav className="mx-auto sm:justify-center space-x-4 text-white">
               {[
                 ['Home', '/'],
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { MainLayout } from "./components/layout"
+import { MainLayout } from "@/app/components/layout"
 
 export default function Home() {
   return <MainLayout><h1>Hello Next.js</h1></MainLayout>
