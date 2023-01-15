d5b1f7e1eea11f7e33ffcd26295f65cffb23ad0f
Jung Gyu Yoon <yjg30737@gmail.com>
2023-01-15 14:40:38 +0900

    Update the footer's color darker

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 export function Footer({ footer } : { footer: string }) {
-  return <footer className="bg-blue-400 p-4 text-center text-white">
-          <h1>{footer}</h1>
+  return <footer className="bg-blue-500 p-4 text-center text-white">
+            <h1>{footer}</h1>
          </footer>
 }
