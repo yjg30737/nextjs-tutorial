f01aa6720e149fd6f1077cef5cfc1f18a8551457
Jung Gyu Yoon <yjg30737@gmail.com>
2023-03-06 16:06:28 +0900

    Update footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,22 +1,22 @@
 import React from "react"
+import Link from "next/link";
 
 export function Footer({ footer } : { footer: string }) {
   return (
     <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">{footer}</a>
-            </span>
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear().toString()} {footer}</span>
             <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                    <Link href="/about" className="mr-4 hover:underline md:mr-6 ">About</Link>
                 </li>
                 <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                    <Link href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
                 </li>
                 <li>
-                    <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+                    <Link href="#" className="mr-4 hover:underline md:mr-6">Licensing</Link>
                 </li>
                 <li>
-                    <a href="#" className="hover:underline">Contact</a>
+                    <Link href="#" className="hover:underline">Contact</Link>
                 </li>
             </ul>
         </footer>
